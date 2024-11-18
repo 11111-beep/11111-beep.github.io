2aab9f6641a0ab11ecb316d0ea2250da72bf28de
11111-beep.github.io <1920634907@qq.com>
2024-11-18 10:54:07 +0800

    Site updated: 2024-11-18 10:54:06

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731897168659";
+const VERSION = "1731898441684";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
