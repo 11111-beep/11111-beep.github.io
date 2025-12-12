8ff2c03268891a4ed0203be7f9bf1ace158696d4
HQW <1920634907@qq.com>
2025-12-12 21:21:54 +0800

    Site updated: 2025-12-12 21:21:48

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1765528445249";
+const VERSION = "1765545701671";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
