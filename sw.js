b1deda49e3cf399a9f5d1e31b66ddbf6c8fab5f9
HQW <1920634907@qq.com>
2025-12-14 15:48:40 +0800

    Site updated: 2025-12-14 15:48:27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765697455123";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765698506411";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
